0d537db29f5ff851db4a61889a4ce36df36804dc
timsayles1 <59299310+timsayles1@users.noreply.github.com>
2023-11-27 13:06:08 -0600

    Update thermostat_update.py

--- thermostat_update.py
+++ thermostat_update.py
@@ -29,6 +29,7 @@ ATTR_HVAC_MODE_DEFAULT = "heat_cool"
 ATTR_FAN_MODE_DEFAULT = "auto"
 ATTR_HIGH_DEFAULT = 73
 ATTR_LOW_DEFAULT = 70
+ATTR_TEMP_DEFAULT = 70
 ATTR_STATE_ONLY_DEFAULT = False
 ATTR_TEMP_ONLY_DEFAULT = False
 
@@ -38,12 +39,12 @@ hvac_state = data.get(ATTR_HVAC_MODE, ATTR_HVAC_MODE_DEFAULT)
 fan_state = data.get(ATTR_FAN_MODE, ATTR_FAN_MODE_DEFAULT)
 high_temp = float(data.get(ATTR_HIGH, ATTR_HIGH_DEFAULT))
 low_temp = float(data.get(ATTR_LOW, ATTR_LOW_DEFAULT))
+target_temp = float(data.get(ATTR_TEMPERATURE, ATTR_TEMP_DEFAULT))
 state_only = data.get(ATTR_STATE_ONLY, ATTR_STATE_ONLY_DEFAULT)
 temp_only = data.get(ATTR_TEMP_ONLY, ATTR_TEMP_ONLY_DEFAULT)
 logger.info("Command state %s temp %s", state_only, temp_only)
 logger.info("Thermostat %s state %s", thermostat_id, hvac_state)
 logger.info("Fan state %s", fan_state)
-logger.info("Temps: High %s Low %s", high_temp, low_temp)
 if state_only and temp_only:
     logger.error("You can't use state_only and temp_only at the same time! Ignoring.")
     state_only = False
@@ -77,7 +78,14 @@ else:
             attributes[ATTR_FAN_MODE] = [fan_state]
         if temp_only:
             state = hass.states.get(thermostat_id).state
-            attributes[ATTR_HIGH] = high_temp
-            attributes[ATTR_LOW] = low_temp
+            attributes[ATTR_HVAC_MODE] = [hvac_state]
+            attributes[ATTR_FAN_MODE] = [fan_state]
+            if hvac_state == 'heat_cool':   
+                attributes[ATTR_HIGH] = high_temp
+                attributes[ATTR_LOW] = low_temp
+                logger.info("Temps: High %s Low %s", high_temp, low_temp)
+            else:
+                attributes[ATTR_TEMPERATURE] = target_temp
+                logger.info("Temps: Target %s", target_temp)
         
         hass.states.set(thermostat_id, state, attributes)
